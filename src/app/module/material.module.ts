917063051fed7177d422a5de5aaa5f0ce83c5aa5
Syphax Djerroud <80024864+bowglue@users.noreply.github.com>
2022-07-11 21:03:37 -0400

    heroku deploy

--- src/app/module/material.module.ts
+++ src/app/module/material.module.ts
@@ -1,71 +1,71 @@
 import { NgModule } from '@angular/core';
 // import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
+//import { MatButtonModule } from '@angular/material/button';
 // import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
+//import { MatCardModule } from '@angular/material/card';
 // import { MatCheckboxModule } from '@angular/material/checkbox';
 // import { MatChipsModule } from '@angular/material/chips';
 // import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
+//import { MatDialogModule } from '@angular/material/dialog';
+//import { MatDividerModule } from '@angular/material/divider';
+//import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+//import { MatIconModule } from '@angular/material/icon';
+//import { MatInputModule } from '@angular/material/input';
 // import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
+//import { MatMenuModule } from '@angular/material/menu';
 // import { MatPaginatorModule } from '@angular/material/paginator';
 // import { MatProgressBarModule } from '@angular/material/progress-bar';
 // import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
+//import { MatRadioModule } from '@angular/material/radio';
+//import { MatSelectModule } from '@angular/material/select';
 // import { MatSidenavModule } from '@angular/material/sidenav';
 // import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 // import { MatSliderModule } from '@angular/material/slider';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+//import { MatSnackBarModule } from '@angular/material/snack-bar';
 // import { MatSortModule } from '@angular/material/sort';
 // import { MatStepperModule } from '@angular/material/stepper';
 // import { MatTableModule } from '@angular/material/table';
 // import { MatTabsModule } from '@angular/material/tabs';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
+//import { MatSidenavModule } from '@angular/material/sidenav';
+//import { MatToolbarModule } from '@angular/material/toolbar';
+//import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 const modules = [
     // MatAutocompleteModule,
-    MatButtonModule,
+    //MatButtonModule,
     // MatButtonToggleModule,
-    MatCardModule,
+   // MatCardModule,
     // MatCheckboxModule,
     // MatCheckboxModule,
     // MatChipsModule,
     // MatDatepickerModule,
-    MatDialogModule,
-    MatExpansionModule,
-    MatFormFieldModule,
+    //MatDialogModule,
+   // MatExpansionModule,
+    MatFormFieldModule
     // MatGridListModule,
-    MatIconModule,
-    MatInputModule,
+    //MatIconModule,
+    //MatInputModule,
     // MatListModule,
-    MatMenuModule,
+   // MatMenuModule,
     // MatPaginatorModule,
     // MatProgressBarModule,
     // MatProgressSpinnerModule,
-    MatRadioModule,
-    MatSelectModule,
+    //MatRadioModule,
+   // MatSelectModule,
     // MatSidenavModule,
     // MatSliderModule,
     // MatSlideToggleModule,
-    MatSnackBarModule,
+    //MatSnackBarModule,
     // MatSortModule,
     // MatStepperModule,
     // MatTableModule,
     // MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatDividerModule,
-    MatSidenavModule,
+    //MatToolbarModule,
+   // MatTooltipModule,
+    //MatDividerModule,
+    //MatSidenavModule,
 ];
 
 /**
